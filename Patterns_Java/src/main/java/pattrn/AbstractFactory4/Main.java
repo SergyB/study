43191058f6e62c89efec412a667a9edf3bc20ad2
Sergy <pochta_61@mail.ru>
2018-02-28 14:17:53 +0300

    loggg

---
+++ Patterns_Java/src/main/java/pattrn/AbstractFactory4/Main.java
@@ -0,0 +1,12 @@
+package pattrn.AbstractFactory4;
+
+public class Main {
+    public static void main(String[] args) {
+        //Шаблоны Java. 4. Абстрактная фабрика (Abstract Factory)
+        //https://www.youtube.com/watch?v=FYX9l5OQtJE&index=5&list=PLwcDaxeEINactCC4mly7RQon5juIpH-Q3
+
+        //Абстрактная фабрика использует внутри себя несколько фабричных методов
+
+        
+    }
+}
